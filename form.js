0b471c7d02db1611bcce7f18b36be7a16940b979
femy99 <47922330+femy99@users.noreply.github.com>
2020-11-21 23:47:11 +0530

    Add files via upload

---
+++ form.js
@@ -0,0 +1,16 @@
+et email = document.getElementById("email");
+let error = document.getElementById("error");
+// document.getElementById("pwd");
+
+function validate(){
+    let regexp = /^([A-Za-z0-9\.-]+)@([A-Za-z0-9\-]+).([a-z]{2,3})(.[a-z]{2,3})?$/
+    if(regexp.test(email.value)){
+        error.innerHTML = "valid";
+        error.style.color = "green"; 
+        return true;
+    }
+    else{
+        error.innerHTML = "invalid";
+        error.style.color = "red";
+        return false;
+    }
